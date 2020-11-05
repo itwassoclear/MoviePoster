f4781a9244f4179ea2067e7b1f40035dfe988032
Maria Kotliarova <itwassoclear@gmail.com>
2020-11-05 10:06:39 +0300

    Updates

---
+++ precache-manifest.642545425029c3ff0ad09c273c8248e5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5302cbe602435abcb2ef5feeb93abc2",
+    "url": "/MoviePoster/index.html"
+  },
+  {
+    "revision": "500cdb684c898636ef83",
+    "url": "/MoviePoster/static/css/2.591cabfe.chunk.css"
+  },
+  {
+    "revision": "947d40e640c50ff54110",
+    "url": "/MoviePoster/static/css/main.fee24045.chunk.css"
+  },
+  {
+    "revision": "500cdb684c898636ef83",
+    "url": "/MoviePoster/static/js/2.5a16f98c.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/MoviePoster/static/js/2.5a16f98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947d40e640c50ff54110",
+    "url": "/MoviePoster/static/js/main.d504c338.chunk.js"
+  },
+  {
+    "revision": "5074c562874a6a88d281",
+    "url": "/MoviePoster/static/js/runtime-main.f1be7b64.js"
+  },
+  {
+    "revision": "8cb62f99fdec448416ae69608e4aa9b5",
+    "url": "/MoviePoster/static/media/arrow.8cb62f99.svg"
+  },
+  {
+    "revision": "610c6d738fb448c379eb46457171ed50",
+    "url": "/MoviePoster/static/media/facebook-logo.610c6d73.svg"
+  },
+  {
+    "revision": "c9791a8a8501543727c9e723f531bd18",
+    "url": "/MoviePoster/static/media/filter-1.c9791a8a.jpg"
+  },
+  {
+    "revision": "3c4c5d8e592d7405409c3df660af8b4b",
+    "url": "/MoviePoster/static/media/filter-2.3c4c5d8e.jpg"
+  },
+  {
+    "revision": "cdb32043da0d41816012941403afba63",
+    "url": "/MoviePoster/static/media/filter-3.cdb32043.jpg"
+  },
+  {
+    "revision": "af5cba8b792d296492542750c06c79c5",
+    "url": "/MoviePoster/static/media/logo.af5cba8b.svg"
+  },
+  {
+    "revision": "601a876988de8559bd6c009ec2e651ab",
+    "url": "/MoviePoster/static/media/telegram-logo.601a8769.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MoviePoster/precache-manifest.642545425029c3ff0ad09c273c8248e5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MoviePoster/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
